3b0fcd4b9332da41d5a48ae4db36edef67d8630e
WDwelle <willdwelle@frontier.com>
2022-01-31 11:47:21 -0800

    added delete button on view one

--- client/src/components/showOne.jsx
+++ client/src/components/showOne.jsx
@@ -7,6 +7,17 @@ const ViewOne = (props) => {
     const {id} = useParams()
 
     const [oneProduct, setOneProduct] = useState({})
+    const [products, setProducts] = useState([]);
+
+    //===DELETE===\\
+    const deleteProduct = (deleteId) => {
+        axios.delete("http://localhost:8000/api/products/" + deleteId)
+            .then( res => {
+                console.log("Success")
+                setProducts(products.filter( (product) => product._id !== deleteId))
+            })
+            .catch( err => console.log(err))
+    }
 
     useEffect( () => {
         axios.get(`http://localhost:8000/api/products/${id}`)
@@ -22,6 +33,7 @@ const ViewOne = (props) => {
         <h2>{oneProduct.title}</h2>
         <h3>{oneProduct.price}</h3>
         <h3>{oneProduct.description}</h3>
+        <button onClick={ () => deleteProduct(product._id)}>DELETE</button>
     </div>
 
 };
