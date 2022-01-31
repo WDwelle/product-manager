5a1e02021bda4114b551ea844d1e89f726244157
Will Dwelle <92066165+WDwelle@users.noreply.github.com>
2022-01-31 12:35:54 -0800

    Delete server directory

--- server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const DB_NAME = "product_manager";
-
-mongoose.connect("mongodb://localhost/" + DB_NAME, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log(`Established a connection to database ${DB_NAME}`))
-    .catch(err => console.log("Something went wrong when connecting to the database", err));
