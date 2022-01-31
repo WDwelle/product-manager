ddc69d4288bf923e115e4a05cfbfd5628003c340
Will Dwelle <92066165+WDwelle@users.noreply.github.com>
2022-01-31 12:36:08 -0800

    Add files via upload

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const DB_NAME = "product_manager";
+
+mongoose.connect("mongodb://localhost/" + DB_NAME, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log(`Established a connection to database ${DB_NAME}`))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 8000;
+
+//middleware
+app.use(express.json(), express.urlencoded({extended:true}));
+app.use(cors());
+
+//connect mongo
+require("./config/mongoose.config");
+//API routes
+require("./routes/product.routes")(app);
+
+//port listener
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
